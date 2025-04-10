f7969ccf2efb03bbc3638fbb5d4575968efdb574
Abhijith Kale <kaleAbhijith@gmail.com>
2025-04-10 09:12:44 +0530

    "updated env"

--- healthstack/settings.py
+++ healthstack/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-GEMINI_API_KEY="AIzaSyAcwIQ3QuRQmKtC8xdn5V1fML2--3uN25Q"
+GEMINI_API_KEY=env('GEMINI_API_KEY')
 
 
 
