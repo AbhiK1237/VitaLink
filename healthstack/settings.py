f4829fcbafcb877767950b2c987f31a0d9bc74ae
Abhijith Kale <kaleAbhijith@gmail.com>
2025-04-10 11:05:56 +0530

    updated allowed hosts

--- healthstack/settings.py
+++ healthstack/settings.py
@@ -36,7 +36,7 @@ GEMINI_API_KEY=env('GEMINI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.0.100', '127.0.0.1','5749-103-109-53-5.in.ngrok.io']
+ALLOWED_HOSTS = ['192.168.0.100', '127.0.0.1','5749-103-109-53-5.in.ngrok.io','totalcare.onrender.com']
 # ALLOWED_HOSTS = ['mobile view', 'local host','ngrok -- keeps on changing']
 
 # Application definition
